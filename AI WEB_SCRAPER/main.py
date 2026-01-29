5eb7107d7de57f1a1be71199d598c659bd3cfdc8
meti2626 <metseboka@gmail.com>
2026-01-29 12:24:07 +0300

    1/29/2026-commit-1

--- AI WEB_SCRAPER/main.py
+++ AI WEB_SCRAPER/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website , extract_body_content, clean_body_content, split_dom_content
 
 
 st.title("AI Web Scraper")
@@ -7,5 +7,12 @@ url = st.text_input("Enter a website URL:")
 
 if st.button("Scrape Site"):
   st.write(f"Scraping the website: {url}")
+  
   result = scrape_website(url)
-  print(result)
+  body_content = extract_body_content(result)
+  cleaned_content = clean_body_content(body_content)
+
+  st.session_state.dom_content = cleaned_content
+
+  with st.expander("Cleaned DOM Content"): 
+      st.text_area("Cleaned Content", cleaned_content, height=300)
--- AI WEB_SCRAPER/scrape.py
+++ AI WEB_SCRAPER/scrape.py
@@ -1,26 +1,56 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 import time 
 
 def scrape_website(website):
-    print("Launching chrome browser....")
+        print("Launching chrome browser....")
 
-    chrome_driver_path = "./chromedriver.exe"  
-    options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")  # Run Chrome in headless mode (optional)
-    # options.add_argument("--disable-gpu")  # Disable GPU acceleration (optional)
-    # options.add_argument("--no-sandbox")  # Bypass OS security model (optional)
+        chrome_driver_path = "./chromedriver.exe"  
+        options = webdriver.ChromeOptions()
+        # options.add_argument("--headless")  # Run Chrome in headless mode (optional)
+        # options.add_argument("--disable-gpu")  # Disable GPU acceleration (optional)
+        # options.add_argument("--no-sandbox")  # Bypass OS security model (optional)
 
-    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+        driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
 
-    try:
-      
+        try:
+        
 
-        driver.get(website)
-        print("Page loaded..")
-        html = driver.page_source
-        time.sleep(50)     
+            driver.get(website)
+            print("Page loaded..")
+            html = driver.page_source
+            time.sleep(50)     
 
-        return html
-    finally:
-        driver.quit()
+            return html
+        finally:
+            driver.quit()
+
+
+
+def extract_body_content(html_content):
+        
+        soup = BeautifulSoup(html_content, 'html.parser')
+        body_content = soup.body
+        
+        if body_content:
+                return str(body_content)
+        return ""
+
+def clean_body_content(body_content):
+        
+        soup = BeautifulSoup(body_content, 'html.parser')
+        
+        for script_or_style in soup(['script', 'style']):
+                script_or_style.extract()
+        
+        cleaned_content = soup.get_text(separator='\n')
+        cleaned_content = "\n".join(
+            line.strip() for line in cleaned_content.splitlines() if line.strip() 
+            )
+        return cleaned_content
+def split_dom_content(dom_content, max_length=600):
+      return [
+        dom_content[i: i + max_length] for i in range(0, len(dom_content), max_length)
+      ]  
+        
