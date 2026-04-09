f057a9e784febdb0d244588d4940f1a64e273dde
meti2626 <metseboka@gmail.com>
2026-04-09 05:22:20 +0300

    4/9/2026

---
+++ AppBloker/logic.py
@@ -0,0 +1,26 @@
+
+HOSTS_PATH = r"C:\Windows\System32\drivers\etc\hosts"
+REDIRECT_IP = "127.0.0.1"
+
+
+def block_website(site):
+
+   with open(HOSTS_PATH , "r+") as file:
+        content = file.read()
+
+        if site not in content:
+            file.write(f"{REDIRECT_IP} {site}\n")
+            file.write(f"{REDIRECT_IP} www.{site}\n")
+
+
+## what the fuck
+
+def unblock_website(site):
+    with open(HOSTS_PATH ,"r") as file:
+        lines = file.readlines()
+
+
+    with open (HOSTS_PATH  , "w") as file:
+        for line in lines:
+             if site not in line:
+                 file.write(line)       
--- AppBloker/ui.py
+++ AppBloker/ui.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from logic import block_website
+
+
 
 def run_app():
   root =tk.Tk()
@@ -17,54 +20,62 @@ def run_app():
       
 
 
-    listbox.insert(tk.END,f"{site} blocked for {time_val} minutes.")  
+    block_website(site)
+    listbox.insert(tk.END, f"{site} - {time_val} min (BLOCKED)") 
+
+## see what we used it for
 
     website_entry.delete(0, tk.END)
     time_entry.delete(0, tk.END)
 
 
 
+  def block_app_ui():
+      app = APP_entry.get()
+      time_val = time_entry.get()
 
+      if not app or not time_val.isdigit():
+        print("Please enter a valid application and time.")
+        return
 
+      listbox.insert(tk.END , f"{app} blocked for {time_val} minutes.")
 
-
-
-
+      APP_entry.delete(0, tk.END)
+      time_entry.delete(0, tk.END)
 
 
 
 # -- Inputs --
 
-tk.Label(root, text = "Website").grid(row=0, column=0 )
-website_entry = tk.Entry(root)
+  tk.Label(root, text = "Website").grid(row=0, column=0 )
+  website_entry = tk.Entry(root)
 
-website_entry.grid(row=0, column=1)
-
-
-tk.Label(root ,text= "Application" ) .grid(row= 1,column =0)
-APP_entry = tk.Entry(root)
-APP_entry.grid(row=1, column=1)
+  website_entry.grid(row=0, column=1)
 
 
-tk.Label(root , text =" Time (minutes) :").grid(row =2 ,column =0)
-time_entry = tk.Entry(root)
-time_entry.grid(row=2, column=1)
-
-# --- Buttons ---
-
-block_site_btn  = tk.Button(root , text = "Block website")
-block_site_btn.grid( row= 3 ,column =0 )
+  tk.Label(root ,text= "Application" ) .grid(row= 1,column =0)
+  APP_entry = tk.Entry(root)
+  APP_entry.grid(row=1, column=1)
 
 
-block_app_btn = tk.Button(root, text="Block App")
-block_app_btn.grid(row=3, column=1)
+  tk.Label(root , text =" Time (minutes) :").grid(row =2 ,column =0)
+  time_entry = tk.Entry(root)
+  time_entry.grid(row=2, column=1)
 
-# --- Listbox ---
-tk.Label(root, text="Active Blocks").grid(row=4, column=0)
+  # --- Buttons ---
+
+  tk.Button(root , text = "Block website" , command =block_website_ui).grid(row=3, column=0)
+  
+  tk.Button(root , text = "Block Application" , command = block_app_ui).grid(row=3, column=1)
 
 
 
-listbox = tk.Listbox(root ,width =  50) 
-listbox.grid(row=5, column=0, columnspan=2)
+  # --- Listbox ---
+  tk.Label(root, text="Active Blocks").grid(row=4, column=0)
 
-root.mainloop()
+
+
+  listbox = tk.Listbox(root ,width =  50) 
+  listbox.grid(row=5, column=0, columnspan=2)
+
+  root.mainloop()
