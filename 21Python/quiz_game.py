378cd1dc7fa1c8c5d350bf1d4c3d1f35fcab69ff
meti2626 <metseboka@gmail.com>
2026-04-17 04:43:14 +0300

    4/17/2026

---
+++ 21Python/quiz_game.py
@@ -0,0 +1,40 @@
+
+print("welcome to this quiz")
+
+playing = input("do you want to play?  ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Lets's play: ")    
+score = 0
+
+answer = input("what does GPU stands for?")
+
+if answer.lower() == "q2":
+     print("correct!")
+     score += 1
+
+else:
+    print("incorrect!")
+
+
+answer = input("what does RAM stands for?")
+
+if answer.lower() == "mem":
+     print("correct!")
+     score += 1
+else:
+    print("incorrect!")
+
+answer = input("what does psu stands for?")
+
+if answer.lower() == "power":
+     print("correct!")
+     score += 1
+else:
+    print("incorrect!")
+
+print ("you got "  + score + " questions correct!")    
+
+
